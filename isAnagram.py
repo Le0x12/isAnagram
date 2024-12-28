e174a6f15fcf1154558ad5ebaf4b63fd7d4d1888
Lex-Os <lexosiris3@gmail.com>
2024-12-27 20:27:01 -0600

    Funciones de diccionario y comparacion
    Se crea el motor para crear el diccionario y para verificar si los pares de palabras son anagramas

---
+++ isAnagram.py
@@ -0,0 +1,34 @@
+
+def diccionarioDeAnagramas():
+  pares = int(input("¿cuantos pares quieres comparar? "))
+  diccionario = {}
+
+  for word in range(pares):
+     key = input("ingresa una palabra " )
+     value = input("ingresa otra palabra " )
+     print("\n")
+     diccionario[key] = value
+
+  return diccionario
+
+def isAnagrama(diccionario):
+   
+   for key in diccionario:
+      palabraUno  = key.lower()
+      palabraDos  =diccionario[key].lower()
+
+      if palabraUno == palabraDos:
+        print ("no es valida la palabra")
+      else:  
+        if sorted(palabraUno) == sorted(palabraDos):
+         print ("\x1b[1;33m" + "*** " + palabraUno + " y " + palabraDos + " son Anagrama --"+"\033[0;m")
+        else:
+         print (palabraUno + " y " + palabraDos + " no son anagrama ")
+      print("\n")
+
+def isAnagramaForDictionary():
+  diccionario = diccionarioDeAnagramas()
+  isAnagrama(diccionario)
+  
+
+isAnagramaForDictionary()
