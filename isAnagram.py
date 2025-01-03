5c2259bcb1abe0e49bbbe41d9a4ffa2e386b4a9e
MonkeyPyLuffy <hbrd1204@gmail.com>
2025-01-03 10:24:30 -0600

    isanagrama v1
    v1 del programa para comparar anagramas,  permite comparar un conjunto de pares definido por el usuario ignorando espacios en blanco y mayusculas.

--- isAnagram.py
+++ isAnagram.py
@@ -1,51 +1,29 @@
-#parte 1
-def diccionarioDeAnagramas():
-  pregunta = str(input("¿quieres comparar pares de palabras?"      ))
-  if pregunta == "si":
-   print  (pares)
-  else:
-    print ("ok de todas formas gracias por interesarte,adios")
-  
-#parte 2
-
-  pares = int(input("¿cuantos pares quieres comparar? "))
+def isAnagrama():
+  pares = int(input("¿cuantos pares quieres comparar? "   ))
   diccionario = {}
 
   for word in range(pares):
      key = input("ingresa una palabra " )
      value = input("ingresa otra palabra " )
-     print("\n")
      diccionario[key] = value
 
-  return diccionario
-
-def isAnagrama(diccionario):
-   
-   for key in diccionario:
+  for key in diccionario:
+    if key == diccionario[key]:
+      print ("no es valida la palabra")
+    else:  
       palabraUno  = key.lower()
       palabraDos  =diccionario[key].lower()
 
       palabraUno = palabraUno.strip()
       palabraDos = palabraDos.strip()
 
-#parte 3
-
-      if palabraUno == palabraDos:
-        print ("no es valida la palabra")
-      else:  
-        if sorted(palabraUno) == sorted(palabraDos):
+      if sorted(palabraUno) == sorted(palabraDos):
          print ("\x1b[1;33m" + "*** " + palabraUno + " y " + palabraDos + " son Anagrama --"+"\033[0;m")
-        else:
+      else:
          print (palabraUno + " y " + palabraDos + " no son anagrama ")
       print("\n")
 
-def isAnagramaForDictionary():
-  diccionario = diccionarioDeAnagramas()
-  isAnagrama(diccionario)
-  
-
-isAnagramaForDictionary()
-
+isAnagrama()
 
 
 
